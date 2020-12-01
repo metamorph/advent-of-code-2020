7c8d96ea0a5ed5b29ee54d9c938fbacf09587e38
Anders Engström <anders.engstrom@leovegas.com>
2020-12-01 20:24:48 +0100

    Clean up the code

--- src/gnejs/day01.clj
+++ src/gnejs/day01.clj
@@ -9,21 +9,10 @@
        (map #(Integer/parseInt %))))
 
 (defn solve [data count target]
-  (let [tuples (combinations data count)]
-    (first
-     (filter
-      (fn [ns] (= target (apply + ns)))
-      tuples))))
+  (->> (combinations data count)
+       (filter (fn [ns] (= target (apply + ns))))
+       (first)))
 
-(defn solve-1 [data]
-  (let [[a b]  (solve data 2 2020)
-        result (* a b)]
-    (printf "%d + %d = %d\n" a b (+ a b))
-    (printf "%d * %d = %d\n" a b result)
-    result))
-(defn solve-2 [data]
-  (let [[a b c] (solve data 3 2020)
-        result  (* a b c)]
-    (printf "%d + %d + %d = %d\n" a b c (+ a b c))
-    (printf "%d * %d * %d = %d\n" a b c result)
-    result))
+(defn solve-1 [data] (apply * (solve data 2 2020)))
+
+(defn solve-2 [data] (apply * (solve data 3 2020)))
