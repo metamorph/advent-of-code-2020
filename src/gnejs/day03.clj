d939d440843c59567a5a028b416b675396e7e160
Anders Engström <anders.engstrom@leovegas.com>
2020-12-03 10:12:03 +0100

    Day 03 - Part 2

--- src/gnejs/day03.clj
+++ src/gnejs/day03.clj
@@ -5,16 +5,25 @@
   ;; Create a matrix of chars
   (mapv vec (read-lines name)))
 
-(defn solve-1 [matrix]
+(defn tree-count [matrix [dx dy :as slope]]
   (let [row-len     (count (first matrix))
         row-count   (count matrix)
         ;; Find the coordinates we will pass through
-        path-coords (take row-count (iterate #(mapv + % [3 1]) [0 0]))
+        path-coords (take row-count (iterate #(mapv + % [dx dy]) [0 0]))
         ;; Collect the terrain on the coordinates
         path        (map (fn [[x y]]
                     (-> (get matrix y)
                         (get (mod x row-len))))
                   path-coords)]
-
     ;; Count how many trees are in the path
     (count (filter #(= % \#) path))))
+
+(defn solve-1 [matrix]
+  (tree-count matrix [3 1]))
+
+(defn solve-2 [matrix]
+  (* (tree-count matrix [1 1])
+     (tree-count matrix [3 1])
+     (tree-count matrix [5 1])
+     (tree-count matrix [7 1])
+     (tree-count matrix [1 2])))
--- test/gnejs/day03_test.clj
+++ test/gnejs/day03_test.clj
@@ -3,6 +3,11 @@
             [clojure.test :as t]))
 
 (t/deftest verify
+
   (t/testing "Sleigh path - Part 1"
     (t/is (= 7 (sut/solve-1 (sut/slope-map "day03_test.txt"))))
-    (t/is (= 203 (sut/solve-1 (sut/slope-map "day03.txt"))))))
+    (t/is (= 203 (sut/solve-1 (sut/slope-map "day03.txt")))))
+
+  (t/testing "Sleigh path - Part 2"
+    (t/is (= 336 (sut/solve-2 (sut/slope-map "day03_test.txt"))))
+    (t/is (= 3316272960 (sut/solve-2 (sut/slope-map "day03.txt"))))))
