30032c735ae1f1d0c3d5cc8d14cd98f9c6b787cf
Anders Engström <anders.engstrom@leovegas.com>
2020-12-06 15:10:40 +0100

    Drop some chars by implementing sum

--- src/gnejs/day06.clj
+++ src/gnejs/day06.clj
@@ -8,11 +8,13 @@
        (partition-by str/blank?)
        (remove (partial = (list "")))))
 
+(defn sum [ns] (reduce + 0 ns))
+
 (defn group-score [join-fn answers]
   (count (apply join-fn (map set answers))))
 
 (defn solve-1 [groups]
-  (reduce + 0 (map (partial group-score union) groups)))
+  (sum (map (partial group-score union) groups)))
 
 (defn solve-2 [groups]
-  (reduce + 0 (map (partial group-score intersection) groups)))
+  (sum (map (partial group-score intersection) groups)))
