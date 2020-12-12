e87650465cd02693b21a14489705be7c2b114383
Anders Engström <anders.engstrom@leovegas.com>
2020-12-12 13:03:58 +0100

    Day12 - Part 1

---
+++ src/gnejs/day12.clj
@@ -0,0 +1,59 @@
+(ns gnejs.day12
+  (:require [clojure.string :refer [join]]
+            [gnejs.aop2020 :refer [read-and-parse]]))
+
+(defn input [name]
+  (read-and-parse name
+                  (fn [line]
+                    (vector (-> (str (first line))
+                                (keyword))
+                            (-> (join (rest line))
+                                (Integer/parseInt))))))
+(defn turn [dir [lr amount]]
+  (let [turn-count (quot amount 90)
+        turns      (case lr
+                     :R (cycle [:N :E :S :W])
+                     :L (cycle [:N :W :S :E]))]
+    (first
+     (drop turn-count
+           (drop-while #(not= % dir) turns)))))
+
+(defn move [{:keys [dir pos] :as state}
+            [action value :as op]]
+  (case action
+    :N (update state :pos #(map + % (map * [0 -1] [1 value])))
+    :S (update state :pos #(map + % (map * [0 1] [1 value])))
+    :E (update state :pos #(map + % (map * [1 0] [value 1])))
+    :W (update state :pos #(map + % (map * [-1 0] [value 1])))
+    :F (move state [dir value])
+    :R (update state :dir #(turn % op))
+    :L (update state :dir #(turn % op))))
+
+(defn move-2 [{:keys [waypoint pos] :as state}
+              [action value :as op]]
+    :N (update state :waypoint #(map + % (map * [0 -1] [1 value])))
+    :S (update state :waypoint #(map + % (map * [0 1] [1 value])))
+    :E (update state :waypoint #(map + % (map * [1 0] [value 1])))
+    :W (update state :waypoint #(map + % (map * [-1 0] [value 1])))
+    :F (update state :pos #(map + % (map * [value value] waypoint)))
+  ;; TODO: Turning
+
+  )
+
+(defn solve-1 [ops]
+  (let [state (reduce move
+                      {:dir :E
+                       :pos [0 0]}
+                      ops)]
+    (println "State:" state)
+    (let [[x y] (:pos state)]
+      (println "Manhattan distance:" (+ (Math/abs x) (Math/abs y))))))
+
+(defn solve-2 [ops]
+  (let [state (reduce move-2
+                      {:waypoint [10 1]
+                       :pos [0 0]}
+                      ops)]
+    (println "State:" state)
+    (let [[x y] (:pos state)]
+      (println "Manhattan distance:" (+ (Math/abs x) (Math/abs y))))))
